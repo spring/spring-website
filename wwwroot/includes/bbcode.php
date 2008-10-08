cb15489c25f105240ba6a3715447c787d293ea7b
fnordia <fnordia@37977431-3df6-0310-b722-df95706aa16b>
2008-10-08 20:24:26 +0000

    Updated bbcode parsing
    git-svn-id: http://springrts.com/svn/spring/site/trunk@6615 37977431-3df6-0310-b722-df95706aa16b

--- wwwroot/includes/bbcode.php
+++ wwwroot/includes/bbcode.php
@@ -3,9 +3,33 @@
     // Should probably use some nice bbcode library
     function parse_bbcode($str)
     {
-        $keys = array('{SMILIES_PATH}', "\n");
-        $data = array('http://spring.clan-sy.com/phpbb/images/smilies', '<br />');
-        return str_replace($keys, $data, $str);
+        $keys = array('/{SMILIES_PATH}/', 
+                      "/\n/",
+                      '/\[i:\w*\]/',
+                      '/\[\/i:\w*\]/',
+                      '/\[url=(.*?):\w*\]/',
+                      '/\[\/url:\w*\]/',
+                      '/\[list:\w*\]/',
+                      '/\[\*:\w*\]/',
+                      '/\[\/\*[:\w]*\]/',
+                      '/\[\/list[:\w]*\]/',
+                      '/\[img:\w*\]/',
+                      '/\[\/img:\w*\]/'
+                      );
+        $data = array('http://spring.clan-sy.com/phpbb/images/smilies',
+                      '<br />',
+                      '<i>',
+                      '</i>',
+                      '<a href="$1">',
+                      '</a>',
+                      '<ul>',
+                      '<li>',
+                      '</li>',
+                      '</ul>',
+                      '<img src="',
+                      '" />'
+                      );               
+        return preg_replace($keys, $data, $str);
     }
   
 ?>
