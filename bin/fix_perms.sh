3b7392d2d3a5ece2029d6293f2c3a06d53c36761
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-11-23 21:36:45 +0100

    script to fix file and directory permissions

---
+++ bin/fix_perms.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+files="
+ wwwroot/thumbs/
+ wwwroot/phpbb/cache/
+ wwwroot/phpbb/files/
+ wwwroot/phpbb/store/
+ wwwroot/phpbb/images/avatars/upload/
+ wwwroot/mediawiki/images/
+"
+
+for f in $files; do
+	if [ ! -e $f ]; then
+		echo "file/directory not found: $f"
+		echo "did you run this script in the right directory?"
+		exit 1
+	fi
+done
+
+chgrp www-data $files
+chmod g+rwx $files
+
+for f in $files; do
+	find $f -type f -exec chmod 660 '{}' \;
+	find $f -type f -exec chgrp www-data '{}' \;
+	find $f -type d -exec chmod 775 '{}' \;
+	find $f -type d -exec chgrp www-data '{}' \;
+done
