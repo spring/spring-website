6b948e4c4cad086bed08642a2990d1923bee7818
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-11-24 20:47:29 +0100

    bbcode parser: made smilies path relative, eat some whitespace around list tags

--- wwwroot/includes/bbcode.php
+++ wwwroot/includes/bbcode.php
@@ -3,8 +3,7 @@
     // Should probably use some nice bbcode library
     function parse_bbcode($str)
     {
-        $keys = array('/{SMILIES_PATH}/', 
-                      "/\n/",
+        $keys = array('/{SMILIES_PATH}/',
                       '/\[b:\w*\]/',
                       '/\[\/b:\w*\]/',
                       '/\[i:\w*\]/',
@@ -12,19 +11,19 @@
                       '/\[url:\w*\](.*?)\[\/url:\w*\]/',
                       '/\[url=(.*?):\w*\]/',
                       '/\[\/url:\w*\]/',
-                      '/\[list:\w*\]/',
-                      '/\[\*:\w*\]/',
-                      '/\[\/\*[:\w]*\]/',
-                      '/\[\/list[:\w]*\]/',
+                      '/\[list:\w*\]\n?/',
+                      '/\[\*:\w*\]\n?/',
+                      '/\[\/\*[:\w]*\]\n?/',
+                      '/\[\/list[:\w]*\]\n?/',
                       '/\[img:\w*\]/',
                       '/\[\/img:\w*\]/',
                       '/\[size=(.*?):\w*\]/',
                       '/\[\/size:\w*\]/',
                       '/\[color=(.*?):\w*\]/',
                       '/\[\/color:\w*\]/',
+                      '/\n/',
                       );
-        $data = array('http://spring.clan-sy.com/phpbb/images/smilies',
-                      '<br />',
+        $data = array('/phpbb/images/smilies',
                       '<b>',
                       '</b>',
                       '<i>',
@@ -42,6 +41,7 @@
                       '',
                       '',
                       '',
+                      '<br />',
                       );
         return preg_replace($keys, $data, $str);
     }
