e9270899bff8643fed3dc8c2f06981b2ab67ea96
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-02-24 11:06:55 +0100

    made fix_perms.sh Red Hat/Fedora aware and added SELinux rules

--- bin/fix_perms.sh
+++ bin/fix_perms.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 files="
- cache/
- wwwroot/thumbs/
- wwwroot/phpbb/cache/
- wwwroot/phpbb/files/
- wwwroot/phpbb/store/
- wwwroot/phpbb/images/avatars/upload/
- wwwroot/mediawiki/images/
+ cache
+ wwwroot/thumbs
+ wwwroot/phpbb/cache
+ wwwroot/phpbb/files
+ wwwroot/phpbb/store
+ wwwroot/phpbb/images/avatars/upload
+ wwwroot/mediawiki/images
 "
+
+if [ -f /etc/redhat-release ]; then
+	group=apache    # Red Hat/Fedora
+else
+	group=www-data  # Debian/Ubuntu
+fi
 
 for f in $files; do
 	if [ ! -e $f ]; then
@@ -18,12 +24,21 @@ for f in $files; do
 	fi
 done
 
-chgrp www-data $files
+chgrp $group $files
 chmod g+rwx $files
 
 for f in $files; do
 	find $f -type f ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chmod 660 '{}' \;
-	find $f -type f ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chgrp www-data '{}' \;
+	find $f -type f ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chgrp $group '{}' \;
 	find $f -type d ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chmod 775 '{}' \;
-	find $f -type d ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chgrp www-data '{}' \;
+	find $f -type d ! \( -name .htaccess -o -name index.\* -o -name \*.php \) -exec chgrp $group '{}' \;
 done
+
+if [ "$1" = "--selinux" ]; then
+	semanage fcontext -a -t httpd_sys_content_t "$PWD/wwwroot(/.*)?"
+	restorecon -R wwwroot
+	for f in $files; do
+		semanage fcontext -a -t httpd_sys_content_rw_t "$PWD/$f(/.*)?"
+		restorecon -R $f
+	done
+fi
