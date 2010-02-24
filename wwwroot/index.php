7219f63bf13622b4c99c05c2b7853a1da04cb573
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-02-24 12:02:44 +0100

    created news-only news.php page by shuffling around some code

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -4,56 +4,13 @@
     include_once('includes/bbcode.php');
     include_once('includes/cache.php');
     include_once('includes/thumbs.php');
+    include_once('includes/news.php');
 
     // Prepare newsitems
-    $sql = "";
-    $sql .= "SELECT t.topic_id, topic_poster, p.post_text, u.username, u.user_email, t.topic_time, t.topic_replies, t.topic_title ";
-    $sql .= "FROM phpbb3_topics AS t, phpbb3_users AS u, phpbb3_posts AS p ";
-    $sql .= "WHERE t.forum_id = 2 AND t.topic_poster = u.user_id AND t.topic_id = p.topic_id AND t.topic_time = p.post_time ";
-    $sql .= "ORDER  BY t.topic_time DESC ";
-    $sql .= "LIMIT 3";
-
-    $res = mysql_query($sql);
-    $newstemplate = file_get_contents('templates/newsitem.html');
-    $news = "";
-    $newskeys = array('#HEADLINE#', '#BODY#', '#POSTER#', '#POSTDATE#', '#COMMENTS#');
-
-    while ($row = mysql_fetch_array($res))
-    {
-        $newstext = parse_bbcode($row['post_text']);
-        $poster = '<a href="/phpbb/memberlist.php?mode=viewprofile&u=' . $row['topic_poster'] . '">' . $row['username'] . '</a>';
-        $postdate = date("Y-m-d H:i", $row['topic_time']);
-        $comments = '<a href="/phpbb/viewtopic.php?t=' . $row['topic_id'] . '">' . $row['topic_replies'] . ' comments</a>.';
-        $newsdata = array($row['topic_title'], $newstext, $poster, $postdate, $comments);
-        $news .= str_replace($newskeys, $newsdata, $newstemplate);
-    }
+    $news = get_news();
 
     // Prepare community headlines
-    $xml = cached_file_get_contents("http://springinfo.info/?feed=rss");
-    try {
-        libxml_use_internal_errors(true);
-        $xml = new SimpleXMLElement($xml);
-    }
-    catch (Exception $ex) {
-        $xml = false;
-    }
-    $cnewstemplate = file_get_contents('templates/cnewsitem.html');
-    $cnews = "";
-    $cnewskeys = array('#HEADLINE#', '#LINK#');
-
-    if ($xml)
-    {
-        foreach ($xml->xpath('/rss/channel/item') as $item)
-        {
-            $newsdata = array((string) $item->title, (string) $item->link);
-            $cnews .= str_replace($cnewskeys, $newsdata, $cnewstemplate);
-        }
-    }
-    if ($cnews == '')
-    {
-        // hack to hide community news altogether if an error occurs
-        $cnews = "<style>.cnews{display:none}</style>";
-    }
+    $cnews = get_community_news();
 
     // Get a random welcome image
     $sql = '';
---
+++ wwwroot/news.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once('includes/news.php');
+
+// Prepare newsitems
+$news = get_news();
+
+// Prepare community headlines
+$cnews = get_community_news();
+
+// Compose the frontpage
+$fptemplate = file_get_contents('templates/news.html');
+$fpkeys = array('#NEWSITEMS#', '#CNEWSITEMS#');
+$fpitems = array($news, $cnews);
+$fp = str_replace($fpkeys, $fpitems, $fptemplate);
+
+// Compose the final page
+$headertemplate = file_get_contents('templates/header.html');
+$starttemplate = file_get_contents('templates/pagestart.html');
+
+$html = $starttemplate;
+//$html .= str_replace('{PAGE_TITLE}', '<img src="/images/homie.gif" width="11" height="10" border="0"/>&nbsp;Home', $headertemplate);
+$html .= $fp;
+//$html .= file_get_contents('templates/footer.html');
+$html .= file_get_contents('templates/pageend.html');
+
+print($html);
+?>
