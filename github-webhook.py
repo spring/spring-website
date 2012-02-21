6827d79e42319b165b403ce6dd0b18ab6405deed
abma <spring@abma.de>
2012-02-21 17:59:42 +0100

    add second github ip

--- github-webhook.py
+++ github-webhook.py
@@ -12,7 +12,7 @@ import subprocess
 import os
 
 HOST_NAME = '0.0.0.0'
-ALLOWED_IPS = { '207.97.227.253' }
+ALLOWED_IPS = { '207.97.227.253', '50.57.128.197' }
 PORT_NUMBER = 9999
 GIT_REPO = os.path.expanduser('~/www')
 LOG_FILE = os.path.expanduser('~/webhook.log')
