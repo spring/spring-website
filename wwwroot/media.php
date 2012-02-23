5a8737255ddb97d7973f39b7c4d853795d200215
-jk- <-jk->
2012-02-23 23:26:20 +0100

    tweak

--- wwwroot/media.php
+++ wwwroot/media.php
@@ -103,7 +103,7 @@
     $tagstr = '';
     if (array_key_exists('tag', $_GET)) {
         $tagstr .= '<li><a href="' . get_link(array('tag' => false, 'play' => false)) . '">Show all content (' . mysql_num_rows($res) . ")</a></li>\n";
-        $tagstr .= "</ul><ul class=\"taglist\">\n";
+        $tagstr .= "</ul><ul class=\"taglist\"><br/>\n";
     }
     foreach ($tags as $tag => $count) {
         $tagstr .= '<li><a href="' . get_link(array('tag' => $tag, 'play' => false)) . '">' . $tag . ' (' . $count . ")</a></li>\n";
