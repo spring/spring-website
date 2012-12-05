9d42b1937f865e19d5fce6ac79e4bd2fd97391d6
abma <spring@abma.de>
2012-12-05 15:46:54 +0100

    fix path

--- wwwroot/mantis/config_inc.php
+++ wwwroot/mantis/config_inc.php
@@ -65,7 +65,7 @@
 	#  in PHP as well.  You may need to add "file_uploads = TRUE" to your php.ini.
 	$g_allow_file_upload	= ON;
         $g_file_upload_method   = DISK;
-	$g_absolute_path_default_upload_folder = '/home/spring/www/wwwroot/mantisbt-1.2.12/uploads';
+	$g_absolute_path_default_upload_folder = $_SERVER['DOCUMENT_ROOT'].'/uploads';
 
 	$g_notify_new_user_created_threshold_min = NOBODY;
 
