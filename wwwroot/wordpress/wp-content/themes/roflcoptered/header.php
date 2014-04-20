2c425bf6d7fd65a35b55339ad925658c06f419f2
Tarendai <contact@tomjn.com>
2014-04-20 12:31:30 +0100

    remove hardcoded strings and make things more robust

--- wwwroot/wordpress/wp-content/themes/roflcoptered/header.php
+++ wwwroot/wordpress/wp-content/themes/roflcoptered/header.php
@@ -13,23 +13,23 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<!-- Place this tag in your head or just before your close body tag. -->
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+	<!-- Place this tag in your head or just before your close body tag. -->
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="icon" type="image/png" href="http://test.springrts.com/wordpress/wp-content/uploads/2013/10/springlogofavicon2.png">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="icon" type="image/png" href="http://test.springrts.com/wordpress/wp-content/uploads/2013/10/springlogofavicon2.png">
 
-<!-- Start Shared Tags Across all Sites -->
+	<!-- Start Shared Tags Across all Sites -->
 	<META NAME="Title" CONTENT="The Spring Project">
 	<meta name="description" content="Spring is a free RTS engine developed for Windows, Linux and Mac OS X!" >
 	<meta name="keywords" content="spring,engine,strategy,tactics,game,design,free,windows,linux,osx,rts,real,time,community,developers,github,oss,open,source,foss">
-<!-- End Shared Tags Across all Sites -->
+	<!-- End Shared Tags Across all Sites -->
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -41,16 +41,32 @@
 		<div class="site-branding">
 			<div class="logopositioning">
 				<div class="logo">
-					<img src="http://test.springrts.com/wordpress/wp-content/uploads/2013/10/springlogoheaderfull2.png" alt="The Spring Project Logo">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="The Spring Project Logo">
 				</div>
 			</div>
-			<?php echo do_shortcode("[metaslider id=38]"); ?>
+			<?php
+			if ( shortcode_exists( 'metaslider' ) ) {
+				$posts = get_posts( array(
+	   				'post_type' => 'ml-slider',
+	   				'name' => 'header'
+	   			));
+	   			if ( empty( $posts ) ) {
+	   				echo '<p>No header slider has been set, go to the meta slider page and create a slider with the name "header"</p>';
+	   			} else {
+	   				foreach ( $posts as $p ) {
+						echo do_shortcode("[metaslider id=".$p->ID."]");
+						break;
+					}
+				}
+			} else {
+				echo '<p>Error: metaslider plugin is not activated</p>';
+			}
+			?>
 		</div>
 	</header><!-- #masthead -->
 </div>
 <div id="topnav">
 	<nav id="site-navigation" class="main-navigation" role="navigation">
-			
 			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'roflcoptered' ); ?>"><?php _e( 'Skip to content', 'roflcoptered' ); ?></a></div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
