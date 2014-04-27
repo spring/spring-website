8102e7bafaf44d9a3f5e142989d32664221b9d61
jK <-jk->
2014-04-27 17:54:14 +0200

    make imagerotate.php (pipes banner image) use http redirect before the browser _always_ reloaded the ~100kB image on rotation no matter if it already downloaded it, or not!

--- wwwroot/imagerotate.php
+++ wwwroot/imagerotate.php
@@ -1,12 +1,9 @@
 <?php
-
-	include_once('includes/db.php');
-
 	// We'd like the image to only be up for rotation every 2 minutes
 	$rot_time = 120;
 	$time_now = gmmktime();
 	$time_now -= ($time_now % $rot_time);
-	header("Etag: " . $time_now);
+	header("Etag: $time_now");
 
 	if (array_key_exists('HTTP_IF_NONE_MATCH', $_SERVER)) {
 		$timestamp = (int)$_SERVER['HTTP_IF_NONE_MATCH'];
@@ -16,10 +13,25 @@
 		}
 	}
 
+	include_once('includes/db.php');
+
 	// Find suitable images to rotate
 	$banners_forum = 32;
 	$sql = '';
 	$sql .= 'select physical_filename, real_filename, topic_title ';
+	$sql .= 'from phpbb3_attachments as a, phpbb3_topics as t ';
+	$sql .= "where t.forum_id = $banners_forum and a.topic_id = t.topic_id ";
+	$sql .= 'order by rand() limit 1';
+
+	$res = mysql_query($sql);
+	if (mysql_num_rows($res) != 1)
+		exit();
+	$row = mysql_fetch_array($res);
+
+	// Find suitable images to rotate
+	$banners_forum = 32;
+	$sql = '';
+	$sql .= 'select physical_filename, real_filename, attach_id ';
 	$sql .= 'from phpbb3_attachments as a, phpbb3_topics as t ';
 	$sql .= "where t.forum_id = $banners_forum and a.topic_id = t.topic_id ";
 	$sql .= 'order by rand() limit 1';
@@ -43,8 +55,8 @@
 	elseif ($picext == 'gif')
 		$mimetype = 'image/gif';
 
+	header("HTTP/1.1 303 See Other");
 	header("Content-Type: $mimetype");
-	header('Content-Length: ' . filesize($fname));
-	readfile($fname);
-	exit();
+	header('Location: http://' . $_SERVER['SERVER_NAME'] . '/phpbb/download/file.php?id=' . $row['attach_id']);
+	//header("Location: http://" . $_SERVER['SERVER_NAME'] . "/$fname");
 ?>
