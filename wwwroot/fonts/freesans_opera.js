932c061354dd85dcf27e6f1c4af771f2560ee9cf
jK <-jk->
2014-05-12 22:58:08 +0200

    workaround a bug in win7+chrome: try to force AA

--- wwwroot/fonts/freesans_opera.js
+++ wwwroot/fonts/freesans_opera.js
@@ -15,3 +15,16 @@ if (window.opera) {
 
 	document.write(overrideWithTTF);
 }
+
+// workaround: chrome on win7 disables antialiasing on webfonts for unknown reason
+if ((window.chrome) && (navigator.appVersion.indexOf("Win") != -1)) {
+	var forceAntiAliasing = function(){/*
+<style>
+html {
+	text-shadow: 0px 0px 0px rgba(0,0,0,0.01);
+}
+</style>
+*/}.toString().slice(14,-3);
+
+	document.write(forceAntiAliasing);
+}
