2d24497abe14c54e2e20518b796ef089d0bfc60f
jK <-jk->
2014-05-14 19:48:38 +0200

    k I am out of ideas: use Arial in case of Chrome+Win

--- wwwroot/fonts/freesans_opera.js
+++ wwwroot/fonts/freesans_opera.js
@@ -16,12 +16,24 @@ if (window.opera) {
 	document.write(overrideWithTTF);
 }
 
-// workaround: chrome on win7 disables antialiasing on webfonts for unknown reason
+// workaround: chrome on windows has a font render bug that makes AA fail (2014)
+// see http://stackoverflow.com/questions/11487427/is-there-any-font-smoothing-in-google-chrome
+// remove when Chrome v35 is out
 if ((window.chrome) && (navigator.appVersion.indexOf("Win") != -1)) {
 	var forceAntiAliasing = function(){/*
 <style>
-html {
-	text-shadow: 0px 0px 1px rgba(0,0,0,0.01);
+@media screen and (-webkit-min-device-pixel-ratio:0) {
+	@font-face {
+		font-family: 'FreeSans Spring';
+		font-weight: 400;
+		src: local(Arial);
+	}
+
+	@font-face {
+		font-family: 'FreeSans Spring';
+		font-weight: bold;
+		src: local(Arial);
+	}
 }
 </style>
 */}.toString().slice(14,-3);
