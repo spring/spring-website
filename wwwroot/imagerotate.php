d51dc7ec51447463cdded28988d7a021a1f5c891
abma <spring@abma.de>
2015-02-06 01:14:52 +0100

    fix header image

--- wwwroot/imagerotate.php
+++ wwwroot/imagerotate.php
@@ -57,4 +57,4 @@
 
 	header("HTTP/1.1 303 See Other");
 	header("Content-Type: $mimetype");
-	header('Location: ' . $_SERVER['SERVER_NAME'] . '/phpbb/download/file.php?id=' . $row['attach_id']);
+	header('Location: ' . '/phpbb/download/file.php?id=' . $row['attach_id']);
