f4286b48be6b07f1a31bc5e720da00dbb766d1d5
ForbodingAngel <forbodingangel@gmail.com>
2015-06-10 21:08:25 -0700

    Add navbar to media page Move meta from meta file into the replacements for mainsite and media Tidy up some things and futureproof them a bit

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -82,7 +82,8 @@
     $metatemplate = file_get_contents('templates/meta.html');
 
     $html  = $starttemplate;
-    $html .= str_replace('{META}', '<link href="/index.css?v=1" rel="stylesheet" type="text/css" />', $metatemplate);
+    $html .= str_replace('{META}', '<link href="/index.css?v=1" rel="stylesheet" type="text/css" />
+									<link href="/header-navbar-mainsite-overrides.css" rel="stylesheet" type="text/css" />', $metatemplate);
     $html .= "<title>Spring RTS Engine</title>\n</head><body>";
     $html .= str_replace('{PAGE_TITLE}', '<img src="/images/homie.gif" width="11" height="10" border="0" alt=""/>&nbsp;Home', $headertemplate);
     $html .= $fp;
