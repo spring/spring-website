d12155a8c41c731f841cebb51037c86ce0a7098b
abma <spring@abma.de>
2015-11-03 11:26:56 +0100

    remove obsolete mysql dump scripts: its to much work to maintain them / keep them useful

--- bin/dump_schema.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# see also: http://bugs.mysql.com/bug.php?id=20786
-mysqldump --skip-dump-date --no-data spring | sed -r '/^\) ENGINE=.*;$/ s/ AUTO_INCREMENT=[0-9]+//g' > mysql/schema.sql
