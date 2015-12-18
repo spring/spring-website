6b1e21f225c68c5d2b8162bfb5101a398b381476
Tom J Nowell <contact@tomjn.com>
2015-12-18 12:08:00 +0000

    Fix up the indenting, remove some servers ( UF died a long time ago )

--- wwwroot/redirect.php
+++ wwwroot/redirect.php
@@ -1,30 +1,21 @@
-<?php {
+<?php
 
- // Redirects incoming requests to the servers defined here
-  $servers[] = array("url" => "http://springrts.com/dl/installer/");
-  $servers[] = array("url" => "http://springmirror.jonanin.com/installer/");
-  $servers[] = array("url" => "http://spring.darkstars.co.uk/installer/");
+// Redirects incoming requests to the servers defined here
+$servers[] = array("url" => "https://springrts.com/dl/installer/");
+$servers[] = array("url" => "http://springmirror.jonanin.com/installer/");
 //  $servers[] = array("url" => "http://64.86.28.46/spring/installer/");
 
 
 // Server overloaded
- //$servers[] = array("url" => "http://evolutionrts.info/installermirror/");
- //$servers[] = array("url" => "http://tasclient.it-l.eu/installer/");
+//$servers[] = array("url" => "http://evolutionrts.info/installermirror/");
+//$servers[] = array("url" => "http://tasclient.it-l.eu/installer/");
 
+if ( !array_key_exists('file', $_GET ) ) {
+	die('Need to provide a filename!');
+}
+$filename = $_GET['file'];
+$server = $servers[ rand( 0, count( $servers ) - 1 )];
 
-// UF down:
-//  $servers[] = array("url" => "http://server6.unknown-files.net/~lordmatt/");
-//  $servers[] = array("url" => "http://buildbot.no-ip.org/~lordmatt/");
-// echo $_GET['file'];
-// exit();
-  if (!array_key_exists('file', $_GET)) 
-    die('Need to provide a filename!');
-
-  $filename = $_GET['file'];
-  $server = $servers[rand(0, count($servers) - 1)];
-
-  $url = $server["url"] . $filename;
-  header("Location: $url");
-  exit();
-
-} ?>
+$url = $server["url"] . $filename;
+header("Location: ".$url);
+exit();
