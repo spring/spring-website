18033a8b3358b2459a238187e74b907009b830c1
abma <spring@abma.de>
2017-04-19 02:57:08 +0200

    split wiki to https://github.com/spring/spring-website-wiki

--- wwwroot/mediawiki/extensions/WikiEditor/modules/ext.wikiEditor.js
+++
@@ -1,7 +0,0 @@
-/*
- * JavaScript for WikiEditor
- */
-jQuery( document ).ready( function ( $ ) {
-	// Initialize wikiEditor
-	$( '#wpTextbox1' ).wikiEditor();
-} );
--- wwwroot/mediawiki/includes/TimestampException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * @since 1.20
- */
-class TimestampException extends MWException {}
